18c82dc353d880492c9b1d597698c06f6eac006e
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-06-16 16:42:31 -0400

    added asset library, updated deps

--- Source/RPGtk.c
+++ Source/RPGtk.c
@@ -1,10 +1,14 @@
 #include <RPGtk.h>
+#include <TKVulkan.h>
 #include <TKWindow.h>
 
 bool tk_initialize(void)
 {
     tkwin_error_t error = tkwin_create();
     if (error != TKWIN_NO_ERROR) return false;
+
+    const char *defaults[2] = {"default.vert", "default.frag"};
+    tkvul_compileShaders(defaults, 2);
 
     return true;
 }
