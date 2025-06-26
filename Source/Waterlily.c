1e82a3990a872293072e36ab1656b410c2155afd
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-06-26 14:24:07 -0400

    added vulkan creation and usage within the init and run functions

--- Source/Waterlily.c
+++ Source/Waterlily.c
@@ -11,12 +11,26 @@ bool waterlily_initialize(const char *title, uint32_t version)
 
     if (!waterlily_vulkanCreate(title, version)) return false;
 
+    void *windowData[2];
+    waterlily_windowGetData(windowData);
+    if (!waterlily_vulkanCreateSurface(windowData)) return false;
+
+    uint32_t width, height;
+    waterlily_windowGetSize(&width, &height);
+    if (!waterlily_vulkanInitialize(width, height)) return false;
+
     return true;
 }
 
 void waterlily_run(void)
 {
-    while (waterlily_windowProcess()) {}
+    uint32_t width, height;
+    waterlily_windowGetSize(&width, &height);
+    while (waterlily_windowProcess())
+    {
+        if (!waterlily_vulkanRenderFrame(width, height)) return;
+        if (!waterlily_vulkanSync()) return;
+    }
 }
 
 void waterlily_cleanup(void) { waterlily_windowDestroy(); }
