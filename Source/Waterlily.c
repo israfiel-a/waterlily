f11bd7672bb3e697554415081cc73cb33f8dff08
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-06-26 15:52:44 -0400

    updated source to actually use hyacinth

--- Source/Waterlily.c
+++ Source/Waterlily.c
@@ -1,10 +1,10 @@
 #include <Geranium.h>
-#include <WLWindow.h>
+#include <Hyacinth.h>
 #include <Waterlily.h>
 
 bool waterlily_initialize(const char *title, uint32_t version)
 {
-    if (!waterlily_windowCreate(title)) return false;
+    if (!hyacinth_create(title)) return false;
 
     const char *defaults[2] = {"default.vert", "default.frag"};
     if (!waterlily_vulkanCompileShaders(defaults, 2)) return false;
@@ -12,11 +12,11 @@ bool waterlily_initialize(const char *title, uint32_t version)
     if (!waterlily_vulkanCreate(title, version)) return false;
 
     void *windowData[2];
-    waterlily_windowGetData(windowData);
+    hyacinth_getData(windowData);
     if (!waterlily_vulkanCreateSurface(windowData)) return false;
 
     uint32_t width, height;
-    waterlily_windowGetSize(&width, &height);
+    hyacinth_getSize(&width, &height);
     if (!waterlily_vulkanInitialize(width, height)) return false;
 
     return true;
@@ -25,12 +25,12 @@ bool waterlily_initialize(const char *title, uint32_t version)
 void waterlily_run(void)
 {
     uint32_t width, height;
-    waterlily_windowGetSize(&width, &height);
-    while (waterlily_windowProcess())
+    hyacinth_getSize(&width, &height);
+    while (hyacinth_process())
     {
         if (!waterlily_vulkanRenderFrame(width, height)) return;
         if (!waterlily_vulkanSync()) return;
     }
 }
 
-void waterlily_cleanup(void) { waterlily_windowDestroy(); }
+void waterlily_cleanup(void) { hyacinth_destroy(); }
