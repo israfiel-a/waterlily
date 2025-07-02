fca43f165114032e4d688667d65f41f24a3abf25
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-07-02 13:25:47 -0400

    renamed all Geranium functions

--- Waterlily.c
+++ Waterlily.c
@@ -9,17 +9,9 @@ bool waterlily_initialize(const char *title, uint32_t version)
     if (!hyacinth_create(title)) return false;
 
     const char *defaults[2] = {"default.vert", "default.frag"};
-    if (!waterlily_vulkanCompileShaders(defaults, 2)) return false;
+    if (!geranium_compileShaders(defaults, 2)) return false;
 
-    if (!waterlily_vulkanCreate(title, version)) return false;
-
-    void *windowData[2];
-    hyacinth_getData(windowData);
-    if (!waterlily_vulkanCreateSurface(windowData)) return false;
-
-    uint32_t width, height;
-    hyacinth_getSize(&width, &height);
-    if (!waterlily_vulkanInitialize(width, height)) return false;
+    if (!geranium_create(title, version)) return false;
 
     primrose_log(SUCCESS, "Initialized engine.");
     return true;
@@ -31,13 +23,14 @@ void waterlily_run(void)
     hyacinth_getSize(&width, &height);
     while (hyacinth_process())
     {
-        if (!waterlily_vulkanRenderFrame(width, height)) return;
-        if (!waterlily_vulkanSync()) return;
+        if (!geranium_render(width, height)) return;
+        if (!geranium_sync()) return;
     }
 }
 
 void waterlily_cleanup(void)
 {
+    geranium_destroy();
     hyacinth_destroy();
     primrose_log(SUCCESS, "Cleaned up engine.");
 }
