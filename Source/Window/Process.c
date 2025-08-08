e90e1fcbd5aee3d2eba7094a32df2183d6533edb
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-08-08 19:05:40 -0400

    found out that the rendering issue was actually cuz i was using a blocking processor for wayland events (closes #6)

--- Source/Window/Process.c
+++ Source/Window/Process.c
@@ -2,7 +2,7 @@
 
 bool waterlily_window_process(waterlily_context_t *context)
 {
-    return wl_display_dispatch(context->window.data.display) != -1 &&
+    return wl_display_dispatch_pending(context->window.data.display) != -1 &&
            !context->window.close;
 }
 
